8af5525fb2b324f4199bff14f1fe345736a67194
LyaysanIsm <53337361+LyaysanIsm@users.noreply.github.com>
2019-11-05 21:54:00 +0400

    change

--- WindowsFormsAirplane/FormParking.cs
+++ WindowsFormsAirplane/FormParking.cs
@@ -73,8 +73,7 @@ namespace WindowsFormsAirplane
         /// Обработка нажатия кнопки "Забрать"   
         /// </summary>       
         /// <param name="sender"></param>         
-        /// <param name="e"></param>    
-
+        /// <param name="e"></param>
         private void buttonTakeAirplane_Click_(object sender, EventArgs e)
         {
             if (maskedTextBox.Text != "")
