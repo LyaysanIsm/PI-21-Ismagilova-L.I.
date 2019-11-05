b49854a93550db4e4a3ead0277dbc4e0d8b51c59
LyaysanIsm <53337361+LyaysanIsm@users.noreply.github.com>
2019-11-05 21:07:10 +0400

    Удалены пустые строки

--- WindowsFormsAirplane/Fighter.cs
+++ WindowsFormsAirplane/Fighter.cs
@@ -93,9 +93,6 @@ namespace WindowsFormsAirplane
                 g.FillEllipse(kl, _startPosX + 98, _startPosY + 120, 5, 30);
                 g.DrawEllipse(pen, _startPosX + 98, _startPosY + 120, 5, 30);
             }
-           
-
-
         }
     }
 }
