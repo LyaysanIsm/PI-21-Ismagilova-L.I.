0f2b5554dbf29c9bc1f285ac0cf93c794e41e964
LyaysanIsm <53337361+LyaysanIsm@users.noreply.github.com>
2019-12-09 12:26:38 +0400

    add

--- WindowsFormsAirplane/Parking.cs
+++ WindowsFormsAirplane/Parking.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Collections;
+using System;
 
 namespace WindowsFormsAirplane
 {
@@ -8,7 +10,8 @@ namespace WindowsFormsAirplane
     /// Параметризованны класс для хранения набора объектов от интерфейса ITransport 
     /// </summary>     
     /// <typeparam name="T"></typeparam>     
-    public class Parking<T> where T : class, ITransport
+    public class Parking<T> : IEnumerator<T>, IEnumerable<T>, IComparable<Parking<T>>
+        where T : class, ITransport
     {
         /// <summary>         
         /// Массив объектов, которые храним      
@@ -40,6 +43,22 @@ namespace WindowsFormsAirplane
         /// </summary>       
         private const int _placeSizeHeight = 80;
 
+        /// <summary>        
+        /// Текущий элемент для вывода через IEnumerator (будет обращаться по своему индексу к ключу словаря, по которму будет возвращаться запись)        
+        /// </summary>        
+        private int _currentIndex;
+
+        /// <summary>    
+        /// Получить порядковое место на парковке        
+        /// </summary>      
+        public int GetKey
+        {
+            get
+            {
+                return _places.Keys.ToList()[_currentIndex];
+            }
+        }
+
         /// <summary>      
         /// Конструктор    
         /// </summary>     
@@ -66,6 +85,10 @@ namespace WindowsFormsAirplane
             if (p._places.Count == p._maxCount)
             {
                 throw new ParkingOverflowException();
+            }
+            if (p._places.ContainsValue(plane))
+            {
+                throw new ParkingAlreadyHaveException();
             }
             for (int i = 0; i < p._maxCount; i++)
             {
@@ -116,10 +139,9 @@ namespace WindowsFormsAirplane
         public void Draw(Graphics g)
         {
             DrawMarking(g);
-            var keys = _places.Keys.ToList();
-            for (int i = 0; i < keys.Count; i++)
+            foreach (var plane in _places)
             {
-                _places[keys[i]].DrawAirplane(g);
+                plane.Value.DrawAirplane(g);
             }
         }
 
@@ -175,5 +197,119 @@ namespace WindowsFormsAirplane
                 }
             }
         }
+
+        /// <summary>     
+        /// Метод интерфейса IEnumerator для получения текущего элемента        
+        /// </summary>         
+        public T Current
+        {
+            get
+            {
+                return _places[_places.Keys.ToList()[_currentIndex]];
+            }
+        }
+
+        /// <summary>      
+        /// Метод интерфейса IEnumerator для получения текущего элемента    
+        /// </summary>    
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        /// <summary>     
+        /// /// Метод интерфейса IEnumerator, вызываемый при удалении объекта        
+        /// /// </summary>       
+        public void Dispose()
+        {
+            _places.Clear();
+        }
+
+        /// <summary> 
+        /// Метод интерфейса IEnumerator для перехода к следующему элементу или началу коллекции      
+        /// </summary>   
+        /// <returns></returns>        
+        public bool MoveNext()
+        {
+            if (_currentIndex + 1 >= _places.Count)
+            {
+                Reset();
+                return false;
+            }
+            _currentIndex++;
+            return true;
+        }
+
+        /// <summary>      
+        /// Метод интерфейса IEnumerator для сброса и возврата к началу коллекции      
+        /// </summary>       
+        public void Reset()
+        {
+            _currentIndex = -1;
+        }
+
+        /// <summary>     
+        /// Метод интерфейса IEnumerable     
+        /// </summary>       
+        /// <returns></returns>       
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        /// <summary>   
+        /// Метод интерфейса IEnumerable      
+        /// </summary>     
+        /// <returns></returns>      
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>     
+        /// Метод интерфейса IComparable       
+        /// </summary>       
+        /// <param name="other"></param>      
+        /// <returns></returns>       
+        public int CompareTo(Parking<T> other)
+        {
+            if (_places.Count > other._places.Count)
+            {
+                return -1;
+            }
+            else if (_places.Count < other._places.Count)
+            {
+                return 1;
+            }
+            else if (_places.Count > 0)
+            {
+                var thisKeys = _places.Keys.ToList();
+                var otherKeys = other._places.Keys.ToList();
+                for (int i = 0; i < _places.Count; ++i)
+                {
+                    if (_places[thisKeys[i]] is Airplane && other._places[thisKeys[i]] is Fighter)
+                    {
+                        return 1;
+                    }
+                    if (_places[thisKeys[i]] is Fighter && other._places[thisKeys[i]] is Airplane)
+                    {
+                        return -1;
+                    }
+                    if (_places[thisKeys[i]] is Airplane && other._places[thisKeys[i]] is Airplane)
+                    {
+                        return (_places[thisKeys[i]] is Airplane).CompareTo(other._places[thisKeys[i]] is Airplane);
+                    }
+                    if (_places[thisKeys[i]] is Fighter && other._places[thisKeys[i]] is Fighter)
+                    {
+                        return (_places[thisKeys[i]] is Fighter).CompareTo(other._places[thisKeys[i]] is Fighter);
+                    }
+                }
+            }
+            return 0;
+        }
     }
 }
+
